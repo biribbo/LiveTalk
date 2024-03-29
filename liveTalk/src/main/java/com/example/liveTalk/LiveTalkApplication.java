f3c4534f90327fd2abcb0c716bb10b274d3dac2b
biribbo <ratajczakbarbara2208@gmail.com>
2024-03-29 13:28:26 +0100

    added rtsp server

--- liveTalk/src/main/java/com/example/liveTalk/LiveTalkApplication.java
+++ liveTalk/src/main/java/com/example/liveTalk/LiveTalkApplication.java
@@ -1,28 +1,10 @@
 package com.example.liveTalk;
-import org.red5.server.adapter.MultiThreadedApplicationAdapter;
-import org.red5.server.api.stream.IBroadcastStream;
-import org.red5.server.api.stream.ISubscriberStream;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiveTalkApplication extends MultiThreadedApplicationAdapter {
-	@Override
-	public void streamBroadcastStart(IBroadcastStream stream) {
-
-	}
-
-	@Override
-	public void streamBroadcastClose(IBroadcastStream stream) {
-
-	}
-
-	@Override
-	public void streamSubscriberStart(ISubscriberStream stream) {
-
-	}
-
-	@Override
-	public void streamSubscriberClose(ISubscriberStream stream) {
-
+public class LiveTalkApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LiveTalkApplication.class, args);
 	}
 }
